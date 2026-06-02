5f10a9f66cca797113f8d6bc588ae0387af57009
Sean Freeman <sfreeman123456789@gmail.com>
2026-06-02 20:42:40 +0530

    remove monorepo files

--- link.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-TARGET="/Users/pcpc/Documents/GitHub/mates"
-LINK="/Users/pcpc/Documents/GitHub/mates/mates-fullstack/example-app/node_modules/mates"
-rm -rf "$LINK"
-ln -s "$TARGET" "$LINK"
-ls -la "$LINK/package.json"
